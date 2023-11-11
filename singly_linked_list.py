327a194f16fde2ac5867c6184feeb4bc02c7eb7e
Rassim Benmhamed <rassimbenmhamed@outlook.com>
2023-11-11 14:27:00 -0600

    Added the print_values method to SList class

--- singly_linked_list.py
+++ singly_linked_list.py
@@ -1,14 +1,25 @@
+# Create a new Python file and recreate the Node and SList classes
 class SLNode:
     def __init__(self,val):
-        self.val = val
+        self.value = val
         self.next = None
 
 class SList:
     def __init__(self):
         self.head = None
-
+    # Add the add_to_front method to your SList class
     def add_to_front(self,val):
         new_node = SLNode(val)
-        new_node.next = self.head
+        current_head = self.head
+        new_node.next = current_head
         self.head = new_node
+        return self
+    # Add the print_values method to your SList class
+    def print_values(self):
+        runner = self.head
+        while (runner != None):
+            print(runner.value)
+            runner = runner.next
+        return self
+
 
